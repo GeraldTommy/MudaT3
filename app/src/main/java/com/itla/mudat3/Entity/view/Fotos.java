9dc0cd76c6e0d9c5b2ad4201bb6f3dd05a2635ff
GeraldTommy <33581578+GeraldTommy@users.noreply.github.com>
2017-12-14 18:32:14 -0400

    AI-3.0.1 <GeraldTommy@GeraldTommyV Overwrite remote https://github.com/GeraldTommy/MudaT3 to local

--- app/src/main/java/com/itla/mudat3/Entity/view/Fotos.java
+++
@@ -1,8 +0,0 @@
-package com.itla.mudat3.Entity.view;
-
-/**
- * Created by GeraldTommy on 28/11/2017.
- */
-
-public class Fotos {
-}
--- app/src/main/java/com/itla/mudat3/Entity/view/TipoUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.itla.mudat3.Entity.view;
-
-/**
- * Created by GeraldTommy on 28/11/2017.
- */
-
-public enum TipoUsuario {
-    PUBLICADOR,CLIENTE
-}
